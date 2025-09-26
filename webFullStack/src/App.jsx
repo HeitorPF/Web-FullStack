872b4deaf36d37266e896d7e2289cb75df7b4698
Brena <114838693+brenaSF@users.noreply.github.com>
2025-09-25 22:06:04 -0300

    Adicionando Comunicação de Componentes com Context API/Redux.

--- webFullStack/src/App.jsx
+++ webFullStack/src/App.jsx
@@ -1,78 +1,25 @@
-import { useState, useRef } from 'react';
+import { useContext, useRef, useEffect } from 'react';
 import loadingImage from './assets/images/loading.gif'
 import './App.css'
 import Header from './components/Header.jsx';
 import ErroModal from './components/ErroModal.jsx';
+import { LyricsContext } from './context/LyricsContext.jsx';
 
 
 function App() {
-  let [lyrics, setLyrics] = useState('')
 
-  const resultado = useRef(null)
+  const { lyrics, modalOpen, errorMessage, loading, fecharModal, buscaMusica } = useContext(LyricsContext);
 
 
-  const [modalOpen, setModalOpen] = useState(false);
-  const [errorMessage, setErrorMessage] = useState('');
+  const resultadoRef = useRef(null);
 
-  const abrirModal = (message) => {
-    setErrorMessage(message);
-    setModalOpen(true);
-  };
-
-  const fecharModal = () => {
-    setModalOpen(false);
-  };
-
-  function validacaoDados(nomeArtista, nomeMusica) {
-    if (!nomeArtista) {
-      abrirModal("Por favor, preencha o nome do artista.");
-      return false;
+  useEffect(() => {
+    if (loading || lyrics) {
+      resultadoRef.current.classList.remove('invi');
+    } else {
+      resultadoRef.current.classList.add('invi');
     }
-
-    if (!nomeMusica) {
-      abrirModal("Por favor, preencha o nome da música.");
-      return false;
-    }
-
-
-    return true;
-  }
-
-
-  async function buscaMusica(nomeArtista, nomeMusica) {
-
-    if (!validacaoDados(nomeArtista, nomeMusica)) {
-      return;
-    }
-    setLyrics('')
-    resultado.current.classList.remove('invi')
-
-
-    const url = `https://lrclib.net/api/search?track_name=${nomeMusica}&artist_name=${nomeArtista}`;
-    fetch(url)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Erro na busca. Tente com outros termos.');
-        }
-        return response.json();
-      })
-      .then(data => {
-        if (data.length === 0) {
-          abrirModal("Nenhuma letra encontrada para essa busca.");
-          setLyrics('');
-          return;
-        }
-        console.log("Resultados encontrados:", data);
-        setLyrics(data[0].plainLyrics);
-      })
-      .catch(error => {
-        console.error("Ocorreu um erro:", error);
-        abrirModal(error.message || 'Ops! Ocorreu um erro ao buscar a letra. Tente novamente mais tarde.');
-        setLyrics('');
-      });
-  }
-
-
+  }, [loading, lyrics]);
 
   return (
     <>
@@ -84,7 +31,7 @@ function App() {
 
       <main>
 
-        <div className='resultado invi ' ref={resultado}>
+        <div className='resultado invi ' ref={resultadoRef}>
           <pre id="lyrics-container">{lyrics ? lyrics : <img src={loadingImage} alt="loading" className='loading-image' />}</pre>
         </div>
 
