543332276563bd40f404ed43b530d7cbc2f7df23
Brena <114838693+brenaSF@users.noreply.github.com>
2025-11-14 15:29:59 -0400

    consertando frontend.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './Home.jsx';
-import Login from './components/Login/Login.js';
+import Login from './components/Login/Login.jsx';
 // import Cadastro from './components/Cadastro/Cadastro.jsx'; 
 import './App.css'; // Mantenha a importação de CSS
-
+import ProtectedRoute from './components/ProtectedRoute.jsx';
+import { BrowserRouter } from 'react-router-dom';
 function AppRouter() {
   return (
     <Routes>
@@ -25,7 +26,7 @@ function AppRouter() {
       {/* <Route path="/cadastro" element={<Cadastro />} /> */}
 
       {/* Você pode adicionar um tratamento para rotas não encontradas (404) aqui, se desejar */}
-    </Routes>
+    </Routes >
   );
 }
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,18 +1,24 @@
+// src/main.jsx (ou index.js)
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'; // Importado
 import './index.css'
-import App from './App.jsx'
+import AppRouter from './App.jsx'
 import { LyricsProvider } from './context/LyricsContext.jsx'
 import { ModalProvider } from './context/ModalContext.jsx';
 
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ModalProvider>
-
-      <LyricsProvider>
-        <App />
-      </LyricsProvider>
-    </ModalProvider>
+    {/* 👈 Adicione basename para corresponder ao prefixo da URL do Vite */}
+    <BrowserRouter basename="/webFullStack">
+      <ModalProvider>
+        <LyricsProvider>
+          <AppRouter />
+        </LyricsProvider>
+      </ModalProvider>
+    </BrowserRouter>
 
   </StrictMode>,
 )
